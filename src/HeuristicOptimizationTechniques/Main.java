225e8690b7650aeeeb6facb24a6e7f63900be394
JonasLechner <lechnerjonas02@gmail.com>
2025-11-20 21:35:36 +0100

    Added InstanceParser

--- src/HeuristicOptimizationTechniques/Main.java
+++ src/HeuristicOptimizationTechniques/Main.java
@@ -1,3 +1,20 @@
-void main() {
+package HeuristicOptimizationTechniques;
 
+import HeuristicOptimizationTechniques.Helper.InstanceParser;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String path = "instances/50/test/instance31_nreq50_nveh2_gamma50.txt";
+
+        InstanceParser parser = new InstanceParser(path);
+
+        System.out.println("Instance name: " + parser.getInstanceName());
+        System.out.println("Requests: " + parser.getNumberOfRequest());
+        System.out.println("Vehicles: " + parser.getNumberOfVehicles());
+        System.out.println("Capacity: " + parser.getVehicleCapacity());
+        System.out.println("Gamma: " + parser.getMinNumberOfRequestsFulfilled());
+        System.out.println("Fairness weight: " + parser.getFairnessWeight());
+    }
 }
+
